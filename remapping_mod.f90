5e9df11381afbadd5cff5fff8405b31ecace4f76
Michael Wenske <m_wens01@wwu.de>
2016-10-06 17:14:25 +0200

    added brucker remapping acord to Chung paper

--- remapping_mod.f90
+++ remapping_mod.f90
@@ -9,6 +9,20 @@ contains
 subroutine remap_stepwise()
   !performs remapping of those fourier space components which will be carried out of the 
   !resolutable area of fourier space 
+ 
+  if(mod(state%step,remapping_rate)==0) then
+    ! TODO: inefficient, since all transforms are conducted, only on dir necessary
+    call transform(state%u_f%val(:,:,1),state%u%val(:,:,1),-1,shearing,sheartime)
+    call transform(state%u_f%val(:,:,2),state%u%val(:,:,2),-1,shearing,sheartime)
+    call transform(state%temp_f%val,state%temp%val,-1,shearing,sheartime)
+    call transform(state%chem_f%val,state%chem%val,-1,shearing,sheartime)
+    sheartime = 0.0_rp
+    call transform(state%u%val(:,:,1),state%u_f%val(:,:,1),+1,shearing,sheartime)
+    call transform(state%u%val(:,:,2),state%u_f%val(:,:,2),+1,shearing,sheartime)
+    call transform(state%temp%val,state%temp_f%val,+1,shearing,sheartime)
+    call transform(state%chem%val,state%chem_f%val,+1,shearing,sheartime)
+  end if
+
 end subroutine
 
 end module
